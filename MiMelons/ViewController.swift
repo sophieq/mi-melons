d5feb760138cf0321d2960bb855b8a1f659edb38
Sophie <sophieeqin@gmail.com>
2019-09-14 13:22:24 -0400

    Initial Commit

---
+++ MiMelons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiMelons
+//
+//  Created by Sophie Qin on 2019-09-14.
+//  Copyright © 2019 Sophie Qin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
